ebd6c4c1569c8dfb55cfa812fa49422c8f0de748
Neil Armitage <flusaka@gmail.com>
2019-03-16 20:06:27 +0000

    Some form of progress towards screenshotting of stats. Needs tidying up.

--- bot.js
+++ bot.js
@@ -1,8 +1,31 @@
-const Discord = require('discord.js');
-const http = require('http');
-const client = new Discord.Client();
+const { Client, Attachment } = require('discord.js');
+const Http = require('http');
+const webshot = require('webshot');
+const client = new Client();
+const fs = require('fs');
 
-http.createServer().listen(process.env.PORT);
+Http.createServer().listen(process.env.PORT);
+
+const sendStats = (channel, username, platform) => {
+    const config = {
+        windowSize: {
+            width: 1920,
+            height: 1080
+        },
+        renderDelay: 1000,
+        timeout: 30000,
+        captureSelector: '.card-table-container'
+    }
+    const url = `https://rocketleague.tracker.network/profile/steam/${username}`;
+    const filename = `${username}_${Date.now().toString()}.png`;
+
+    webshot(url, filename, config, (err) =>  {
+        const attachment = new Attachment(filename, filename);
+        channel.send(attachment).then(() => {
+            fs.unlink(filename, () => {});
+        });
+    });
+}
 
 client.on("message", (message) => {
     if(message.author.bot) {
@@ -10,8 +33,12 @@ client.on("message", (message) => {
     }
 
     const { content, channel } = message;
-    if(content === "!rlstats") {
+    if(content.startsWith("!rlstats")) {
+        const params = content.split(' ').slice(1);
+        console.log(content);
         channel.send("Retrieving stats...");
+        
+        sendStats(channel, params[0]);
     }
 });
 
